a60313dcc2543b04fa7a0c470e95c1d34e4c8ce6
Sim <simshard@gmail.com>
2025-06-23 03:06:51 +0100

    removed Laravel Auth  &Settings tests

--- tests/Feature/Pages/PageResponseTest.php
+++ tests/Feature/Pages/PageResponseTest.php
@@ -3,12 +3,12 @@
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
-test('home page returns a successful response', function () {
-    $response = $this->get(route('home'));
-    $response->assertOk();
-});
+// test('home page returns a successful response', function () {
+//     $response = $this->get(route('home'));
+//     $response->assertOk();
+// });
 
-test('course details page returns a successful response', function () {
-    $response = $this->get(route('course-details', 1));
-    $response->assertOk();
-});
+// test('course details page returns a successful response', function () {
+//     $response = $this->get(route('course-details', 1));
+//     $response->assertOk();
+// });
